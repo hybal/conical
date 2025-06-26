f8d967003c3fca88bc38e66f0a71465609b1a33e
ellipse12 <ellipse1234567@gmail.com>
2025-06-25 20:23:20 -0600

    lets keep the main branch somewhat working

--- src/Hir.zig
+++ src/Hir.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Ast = @import("Ast.zig");
+const types = @import("types.zig");
 
 const BinaryOp = enum {
     Add,
@@ -34,9 +35,14 @@ const UnaryOp = enum {
 };
 
 const BinaryExpr = struct {
-    lhs: HirNode,
-    rhs: HirNode,
+    lhs: Hir,
+    rhs: Hir,
     op: BinaryOp,
+};
+
+const UnaryExpr = struct {
+    op: UnaryOp,
+    expr: Hir,
 };
 
 const Terminal = union(enum) {
@@ -48,22 +54,48 @@ const Terminal = union(enum) {
     unit,
 };
 
-const UnaryExpr = struct {
-    op: UnaryOp,
-    expr: HirNode,
-};
 
 const Assignment = struct {
-    lvalue: HirNode,
-    expr: HirNode,
+    lvalue: Hir,
+    expr: Hir,
 };
 
-//This should always be pointer sized
+const Block = struct {
+    exprs: []Hir,
+};
+
+const Loop = Block;
+
+const Conditional = struct {
+    condition: Hir,
+    then_block: Hir,
+    else_block: ?Hir,
+};
+
+const Cast = struct {
+    expr: Hir,
+    to_type: Ast.TypeId,
+};
+
+
+const FnCall = struct {
+
+};
+
+//NOTE: This should always be pointer sized
 const HirNode = union(enum) {
     binary_expr: *BinaryExpr,
     unary_expr: *UnaryExpr,
     terminal: *Terminal,
     assignment: *Assignment,
-    
+    block: *Block,
+    loop: *Loop,
+    conditional: *Conditional,
+    cast: *Cast,
+};
 
+const Hir = struct {
+    tyid: Ast.TypeId,
+    span: types.Span,
+    node: HirNode,
 };
