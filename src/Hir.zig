0820112b43b679094daa01325fcaae1f19a1d31f
ellipse12 <ellipse1234567@gmail.com>
2025-07-02 15:44:50 -0600

    started moving to HIR instead of direct use of the AST

--- src/Hir.zig
+++ src/Hir.zig
@@ -1,7 +1,9 @@
 const std = @import("std");
 const Ast = @import("Ast.zig");
+const types = @import("types.zig");
+const mem = @import("mem.zig");
 
-const BinaryOp = enum {
+pub const BinaryOp = enum {
     Add,
     Sub,
     Mul,
@@ -25,7 +27,7 @@ const BinaryOp = enum {
     Cast,
 };
 
-const UnaryOp = enum {
+pub const UnaryOp = enum {
     Not,
     BinNot,
     DeRef,
@@ -33,37 +35,74 @@ const UnaryOp = enum {
     Minus,
 };
 
-const BinaryExpr = struct {
-    lhs: HirNode,
-    rhs: HirNode,
+pub const BinaryExpr = struct {
+    lhs: Hir,
+    rhs: Hir,
     op: BinaryOp,
 };
 
-const Terminal = union(enum) {
+pub const Ident = struct {
+    location: types.Span,
+    value: []const u8,
+};
+
+pub const Terminal = union(enum) {
     string_literal: []const u8, 
     integer_literal: u128,
     float_literal: f64, 
-    identifier: []const u8, 
+    identifier: Ident, 
     type_literal: *Ast.Type,
     unit,
 };
 
-const UnaryExpr = struct {
+pub const UnaryExpr = struct {
     op: UnaryOp,
-    expr: HirNode,
+    expr: Hir,
 };
 
-const Assignment = struct {
-    lvalue: HirNode,
-    expr: HirNode,
+pub const Assignment = struct {
+    lvalue: Hir,
+    expr: Hir,
+};
+pub const Block = struct {
+    body: []Hir,
 };
 
-//This should always be pointer sized
-const HirNode = union(enum) {
+pub const Loop = struct {
+    block: Block,
+};
+
+pub const FnDecl = struct {
+    id: Ident,
+
+};
+
+pub const Hir = struct {
+    node: HirNode,
+    typeid: ?Ast.TypeId = null,
+    escapes: bool = false,
+    span: types.Span,
+
+    pub fn create(node: HirNode, span: types.Span) @This() {
+        return .{
+            .node = node,
+            .span = span,
+        };
+    }
+};
+
+//This should always be pointer sized + the tag size
+pub const HirNode = union(enum) {
     binary_expr: *BinaryExpr,
     unary_expr: *UnaryExpr,
     terminal: *Terminal,
     assignment: *Assignment,
-    
+    block: *Block,
+    fn_decl: *FnDecl,
+    fn_call: *FnCall,
+    return_stmt: *Return,
+    cast: *Cast,
+    loop: *Loop,
+    branch: *Branch,
 
 };
